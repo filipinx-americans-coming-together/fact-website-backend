bcce09ace652394255f1c9948696e66eaf2d7cc6
Kian <kianchristiand@gmail.com>
2024-07-14 10:35:20 -0500

    changed request retrieval from form-data to JSON in views and noted a few things to work on

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse, get_object_or_404
 from django.http import JsonResponse
 from django.core import serializers
 from registration.models import Workshop, Location
@@ -6,41 +6,70 @@ from django.views.decorators.csrf import csrf_exempt
 
 import json
 
+"""
+PROBLEMS WITH CODE:
+1. Remove csrf_exempt from all functions
+2. PUT requests use request.body instead of request.POST (may need to use raw data for all requests)
+3. Figure out relationship between Workshop and Location
+"""
+
 @csrf_exempt
 def workshop(request):
+    # Note: Does not account for when attributes are missing in POST request
     if request.method == "POST":
-        workshop = Workshop.objects.create(title=request.POST.get("title"), description=request.POST.get("description"), facilitators=request.POST.get("facilitators"), location=request.POST.get("location"), session=request.POST.get("session"))
-        workshop.save()
-        
-        return HttpResponse(status=200)
+        try:
+            data = json.loads(request.body)
+            location = get_object_or_404(Location, id=data.get("location"))
+
+            w = Workshop.objects.filter(title=data.get("title"))
+
+            if (w.exists()):
+                if (w[0].session == data.get("session")):    
+                    return HttpResponse("Workshop in current session already exists")
+
+            workshop = Workshop(
+                title=data.get("title"),
+                description=data.get("description"),
+                facilitators=data.get("facilitators"),
+                location=location,
+                session=data.get("session")
+            )
+
+            workshop.save()
+            return HttpResponse(status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON"}, status=400)
     elif request.method == "GET":
         data = serializers.serialize('json', Workshop.objects.all())
         return HttpResponse(data, content_type="application/json")
     else:
         return HttpResponse(status=400)
-    
+
 @csrf_exempt
 def workshop_id(request, id):
+    workshop = get_object_or_404(Workshop, location_id=id)
+
     if request.method == "GET":
-        data = serializers.serialize('json', Workshop.objects.filter(id=id))
+        data = serializers.serialize('json', [workshop])
         return HttpResponse(data, content_type="application/json")
     elif request.method == "PUT":
         try:
+            # Link between Workshop and Location changes pk of workshop when location is changed
             data = json.loads(request.body)
-            Workshop = Workshop.objects.get(id=id)
-            Workshop.title = data.get("title", Workshop.title)
-            Workshop.description = data.get("description", Workshop.description)
-            Workshop.facilitators = data.get("facilitators", Workshop.facilitators)
-            Workshop.location = data.get("location", Workshop.location)
-            Workshop.session = data.get("session", Workshop.session)
-            Workshop.save()
+            location = get_object_or_404(Location, id=data.get("location"))
+
+            workshop.title = data.get("title", workshop.title)
+            workshop.description = data.get("description", workshop.description)
+            workshop.facilitators = data.get("facilitators", workshop.facilitators)
+            workshop.location = location
+            workshop.session = data.get("session", workshop.session)
+
+            workshop.save()
             return HttpResponse(status=200)
-        except Workshop.DoesNotExist:
-            return JsonResponse({"error": "Workshop not found"}, status=404)
         except json.JSONDecodeError:
             return JsonResponse({"error": "Invalid JSON"}, status=400)
     elif request.method == "DELETE":
-        Workshop.objects.filter(id=id).delete()
+        workshop.delete()
         return HttpResponse(status=200)
     else:
         return HttpResponse(status=400)
@@ -48,13 +77,19 @@ def workshop_id(request, id):
 @csrf_exempt
 def location(request):
     if request.method == "POST":
-        obj1 = Location.objects.filter(room_num=request.POST.get("room_num"))
-        obj2 = Location.objects.filter(building=request.POST.get("building"))
+        data = json.loads(request.body)
+        obj1 = Location.objects.filter(room_num=data.get("room_num"))
+        obj2 = Location.objects.filter(building=data.get("building"))
         if (obj1.exists() and obj2.exists()):
             if (obj1[0].id == obj2[0].id):
                 return HttpResponse("Location already exists")
 
-        location = Location.objects.create(room_num=request.POST.get("room_num"), building=request.POST.get("building"), capacity=request.POST.get("capacity"))
+        location = Location.objects.create(
+            room_num=data.get("room_num"),
+            building=data.get("building"),
+            capacity=data.get("capacity")
+        )
+        
         location.save()
 
         return HttpResponse(status=200)
@@ -73,9 +108,11 @@ def location_id(request, id):
         try:
             data = json.loads(request.body)
             location = Location.objects.get(id=id)
+
             location.room_num = data.get("room_num", location.room_num)
             location.building = data.get("building", location.building)
             location.capacity = data.get("capacity", location.capacity)
+
             location.save()
             return HttpResponse(status=200)
         except Location.DoesNotExist:
