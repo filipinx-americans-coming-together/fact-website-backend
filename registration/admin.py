9099184177504fea4adfb90e7ca29db3fefcf579
Kian <kianchristiand@gmail.com>
2024-08-22 19:48:27 -0500

    added Facilitator model to admin.py

--- registration/admin.py
+++ registration/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Workshop, Location, School, Delegate, Registration
+from .models import Workshop, Location, Facilitator, School, Delegate, Registration
 
 admin.site.register(Workshop)
 admin.site.register(Location)
+admin.site.register(Facilitator)
 admin.site.register(School)
 admin.site.register(Delegate)
 admin.site.register(Registration)
