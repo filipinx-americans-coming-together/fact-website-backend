b982892ea07c3f6e47e7bc693cdc3cd78c934215
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-17 20:43:45 -0500

    added num registrations for workshop endpoint

--- registration/workshop/tests.py
+++ registration/workshop/tests.py
@@ -2,38 +2,40 @@ import json
 
 from django.test import Client, TestCase
 from django.urls import reverse
-from registration.models import Location, Workshop
+from django.contrib.auth.models import User
+
+from registration.models import Delegate, Location, Registration, Workshop
 
 
 class WorkshopAPITestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        self.location = Location.objects.create(room_num="Test Room", building="Test Building", capacity=100)
+        self.location = Location.objects.create(
+            room_num="Test Room", building="Test Building", capacity=100
+        )
         self.workshop = Workshop.objects.create(
             title="Test Workshop",
             description="Test Description",
-            facilitators=[
-                "Facilitator 1",
-                "Facilitator 2"
-            ],
+            facilitators=["Facilitator 1", "Facilitator 2"],
             location=self.location,
-            session=1
+            session=1,
         )
-        self.workshop_url = reverse('registration:workshop')
-        self.workshop_id_url = reverse('registration:workshop_id', kwargs={'id': self.workshop.pk})
+        self.workshop_url = reverse("registration:workshop")
+        self.workshop_id_url = reverse(
+            "registration:workshop_id", kwargs={"id": self.workshop.pk}
+        )
 
     def test_post_workshop(self):
         data = {
             "title": "New Workshop",
             "description": "New Description",
-            "facilitators": [
-                "Facilitator 1",
-                "Facilitator 2"
-            ],
+            "facilitators": ["Facilitator 1", "Facilitator 2"],
             "location": self.location.id,
-            "session": 1
+            "session": 1,
         }
-        response = self.client.post(self.workshop_url, json.dumps(data), content_type="application/json")
+        response = self.client.post(
+            self.workshop_url, json.dumps(data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
 
     # def test_post_workshop_missing_attribute(self):
@@ -50,7 +52,9 @@ class WorkshopAPITestCase(TestCase):
     #     self.assertEqual(response.status_code, 400)
 
     def test_post_workshop_invalid_json(self):
-        response = self.client.post(self.workshop_url, "Invalid JSON", content_type="application/json")
+        response = self.client.post(
+            self.workshop_url, "Invalid JSON", content_type="application/json"
+        )
         self.assertEqual(response.status_code, 400)
         self.assertContains(response, "Invalid JSON", status_code=400)
 
@@ -68,14 +72,13 @@ class WorkshopAPITestCase(TestCase):
         data = {
             "title": "Updated Workshop",
             "description": "Updated Description",
-            "facilitators": [
-                "Updated Facilitator 1",
-                "Updated Facilitator 2"
-            ],
+            "facilitators": ["Updated Facilitator 1", "Updated Facilitator 2"],
             "location": self.location.id,
-            "session": 1
+            "session": 1,
         }
-        response = self.client.put(self.workshop_id_url, json.dumps(data), content_type="application/json")
+        response = self.client.put(
+            self.workshop_id_url, json.dumps(data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
         self.workshop.refresh_from_db()
         self.assertEqual(self.workshop.title, "Updated Workshop")
@@ -90,13 +93,70 @@ class WorkshopAPITestCase(TestCase):
         data = {
             "title": "Test Workshop",
             "description": "Another Description",
-            "facilitators": [
-                "Facilitator 1",
-                "Facilitator 2"
-            ],
+            "facilitators": ["Facilitator 1", "Facilitator 2"],
             "location": self.location.id,
-            "session": 1
+            "session": 1,
         }
-        response = self.client.post(self.workshop_url, json.dumps(data), content_type="application/json")
+        response = self.client.post(
+            self.workshop_url, json.dumps(data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Workshop in current session already exists")
+
+
+class WorkshopRegistrations(TestCase):
+    # TODO fix/remove location after merge
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_404s_no_workshop(self):
+        location = Location.objects.create(
+            room_num="A", building="building", capacity=10, session=1
+        )
+
+        workshop = Workshop.objects.create(
+            title="workshop title",
+            description="workshop description",
+            facilitators="[fac1, fac2]",
+            session=1,
+            location=location,
+        )
+
+        response = self.client.get(
+            reverse(
+                "registration:workshop_registration", kwargs={"id": workshop.pk + 1}
+            )
+        )
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_returns_num_registrations(self):
+        location = Location.objects.create(
+            room_num="A", building="building", capacity=10, session=1
+        )
+
+        workshop = Workshop.objects.create(
+            title="workshop title",
+            description="workshop description",
+            facilitators="[fac1, fac2]",
+            session=1,
+            location=location,
+        )
+
+        expected_registrations = 3
+
+        for i in range(expected_registrations):
+            user = User.objects.create(username=f"user{i}")
+            delegate = Delegate.objects.create(user=user, year="")
+
+            Registration.objects.create(workshop=workshop, delegate=delegate)
+
+        response = self.client.get(
+            reverse(
+                "registration:workshop_registration", kwargs={"id": workshop.pk}
+            )
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["num_registrations"], expected_registrations)
