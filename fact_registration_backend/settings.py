1432b4546dc31d775520476002ce6b274d0513ab
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-26 22:47:21 -0500

    removed v show

--- fact_registration_backend/settings.py
+++ fact_registration_backend/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'fact_admin',
     'one_time_verification',
-    'variety_show',
     'registration',
     'corsheaders',
 ]
--- variety_show/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'variety_show'
-urlpatterns = [
-    path('tickets/', views.tickets, name='tickets'),
-]
