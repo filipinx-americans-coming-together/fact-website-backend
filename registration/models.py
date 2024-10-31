82225b0fd0748a5405d7bd8b2f5cdd4aed841168
Alyssa Daniels <asagedaniels@gmail.com>
2024-10-31 15:25:21 -0500

    modified workshops to use FacilitatorWorkshop relation instead of facilitator list

--- registration/models.py
+++ registration/models.py
@@ -28,7 +28,6 @@ class Facilitator(models.Model):
 class Workshop(models.Model):
     title = models.CharField(max_length=100, default="")
     description = models.TextField()
-    facilitators = models.JSONField(default=list)
     location = models.OneToOneField(
         Location, on_delete=models.CASCADE, null=True, blank=True
     )
